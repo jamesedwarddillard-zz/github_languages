8d43ba190d389cc1ef942d2bbab0f80f230e48bd
James Edward Dillard <jamesedwarddillard@gmail.com>
2014-08-19 09:37:45 -0400

    added get languages function

--- github_languages.py
+++ github_languages.py
@@ -2,7 +2,7 @@ import sys
 import operator
 from collections import defaultdict
 import requests
-from secret import USERNAME, PASSWORD
+from secrets import USERNAME, PASSWORD
 
 def get_repositories(user):
 	"""Retrieve a list of a user's repositories"""
@@ -10,9 +10,23 @@ def get_repositories(user):
 	response = requests.get(url, auth=(USERNAME, PASSWORD))
 	return response.json()
 
+def get_language_dictionaries(repositories):
+    """
+    Return a list of dictionaries containing the languages used in each
+    repository
+    """
+    language_dictionaries = []
+    for repository in repositories:
+        url = "https://api.github.com/repos/{owner}/{repo}/languages"
+        url = url.format(owner=repository["owner"]["login"], repo=repository["name"])
+        response = requests.get(url, auth=(USERNAME, PASSWORD))
+        language_dictionaries.append(response.json())
+    return language_dictionaries
+
 def main():
 	repositories = get_repositories(sys.argv[1])
-	print repositories
+	language_dictionaries = get_language_dictionaries(repositories)
+	print language_dictionaries
 
 if __name__ == "__main__":
 	main()
