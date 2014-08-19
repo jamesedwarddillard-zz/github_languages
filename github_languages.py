fdcd73859a111951bdc25903ce5e5d9153cada6c
James Edward Dillard <jamesedwarddillard@gmail.com>
2014-08-19 09:53:13 -0400

    created a function to accumulate language totals and edited the main fuction to provide % by language

--- github_languages.py
+++ github_languages.py
@@ -23,10 +23,26 @@ def get_language_dictionaries(repositories):
         language_dictionaries.append(response.json())
     return language_dictionaries
 
+def accumulate_languages(language_dictionaries):
+	"""Calculate the total data size for each language"""
+	accumulated = defaultdict(int)
+	total = 0
+	for language_dictionary in language_dictionaries:
+		for language_name, number_of_bytes in language_dictionary.iteritems():
+			accumulated[language_name] += number_of_bytes
+			total += number_of_bytes
+	return accumulated, total
+
 def main():
 	repositories = get_repositories(sys.argv[1])
 	language_dictionaries = get_language_dictionaries(repositories)
-	print language_dictionaries
+	language_totals, total_bytes = accumulate_languages(language_dictionaries)
+
+	sorted_language_totals = sorted(language_totals.iteritems(), key=operator.itemgetter(1), reverse = True)
+
+	for language_name, number_of_bytes in sorted_language_totals: 
+		percentage = 100.0 * number_of_bytes / total_bytes
+		print "{}: {:.2f}%".format(language_name, percentage)
 
 if __name__ == "__main__":
 	main()
