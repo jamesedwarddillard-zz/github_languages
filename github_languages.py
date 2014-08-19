c3f8c3a15c6dc4c4961d8f7bdfd3f7f87dd0d6fb
James Edward Dillard <jamesedwarddillard@gmail.com>
2014-08-19 08:55:59 -0400

    added an authorization

--- github_languages.py
+++ github_languages.py
@@ -2,11 +2,12 @@ import sys
 import operator
 from collections import defaultdict
 import requests
+from secret import USERNAME, PASSWORD
 
 def get_repositories(user):
 	"""Retrieve a list of a user's repositories"""
 	url = "https://api.github.com/users/{user}/repos".format(user=user)
-	response = requests.get(url)
+	response = requests.get(url, auth=(USERNAME, PASSWORD))
 	return response.json()
 
 def main():
@@ -15,4 +16,3 @@ def main():
 
 if __name__ == "__main__":
 	main()
-	
